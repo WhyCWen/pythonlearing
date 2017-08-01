b5af5a78a6b0706ce6f0ead05470fd006f7e5ddd
Hong <8720whyc@gmail.com>
2017-08-01 20:31:23 +0800

    whyc add two file

---
+++ DemoTwo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2  
+# encoding: utf-8  
+
+""" 
+@version: v1.0 
+@author: whyc
+@license: Apache Licence  
+@contact: 8720whyc@gmail.com
+@site:  
+@software: PyCharm 
+@file: DemoTwo.py 
+@time: 17-8-1 下午8:30 
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass  
